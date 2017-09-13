36259cab48b1e790b9476fe645aa9f63356ac58e
George Y. Kussumoto <georgeyk.dev@gmail.com>
2017-09-13 20:41:15 -0300

    Initial updates on sponsors screen
    Added ScrollView and ajustments in related widgets.

--- pybr/uix/screens/screensponsor.py
+++ pybr/uix/screens/screensponsor.py
@@ -3,11 +3,12 @@ from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 from network import get_data
 from kivy.factory import Factory
-from kivy.uix.stacklayout import StackLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.scrollview import ScrollView
 
 
-class Sponsor(StackLayout):
-    '''This is a simple StackLayout that holds the image'''
+class Sponsor(BoxLayout):
+    '''This is a simple BoxLayout that holds the image'''
     data = ObjectProperty(None)
 
 
@@ -16,55 +17,57 @@ class ScreenSponsor(Screen):
     Builder.load_string('''
 <ScreenSponsor>
     name: 'ScreenSponsor'
-    BoxLayout
-        orientation: 'vertical'
-        spacing: dp(4)
-        id:main
-        BackLabel
-            text: "Sponsors"
-            backcolor: app.base_inactive_color[:3] + [.5]
-            pos_hint:{'top':1}
-            size_hint: 1 , None
-            height: dp(10)
-            font_size:dp(20)
+    ScrollView
+        ScrollGrid
+            BackLabel
+                text: "Patrocinadores"
+                backcolor: app.base_inactive_color[:3] + [.5]
+                pos_hint: {'top':1}
+                size_hint: 1 , None
+                font_size: dp(20)
+            GridLayout
+                id: main
+                rows: 5
+                cols: 2
+                row_default_height: 150
+                size_hint_y: None
+                allow_strech: True
+                padding: '9dp'
+                spacing: '9dp'
+            BoxLayout:
+                id: footer_box
+                size_hint_y: None
+                padding: dp(9)
+                spacing: dp(9)
 
-<Footer@BoxLayout>
-    size_hint_y: .2
-    padding: dp(9)
-    spacing: dp(9)
-    ActiveButton
-        text: 'Patrocine-nos'
-        size_hint_y: None
-        height: dp(40)
-        on_release:
-            import webbrowser
-            webbrowser.open('https://docs.google.com/presentation/d/1-8u0QWpi5zL21zZ9xehx0wxebXHi5zE0hKNbBtU4ncI/pub?start=false&loop=false&delayms=3000')
+<Footer@ActiveButton>
+    text: 'Patrocine-nos'
+    size_hint_y: None
+    height: dp(40)
+    on_release:
+        import webbrowser
+        webbrowser.open('https://docs.google.com/presentation/d/1-8u0QWpi5zL21zZ9xehx0wxebXHi5zE0hKNbBtU4ncI/pub?start=false&loop=false&delayms=3000')
 
 <Sponsor>
-    orientation: 'tb-rl'
+    orientation: 'vertical'
     spacing: dp(12)
     size_hint: 1, 1
+    SponsorImage
+        size_hint: 1,.8
+        allow_stretch: False
+        halign: 'center'
+        valign: 'middle'
+        padding: dp(9), dp(9)
     BackLabel
         text: self.parent.data['name']
         size_hint: 1, None
-        height: dp(20)
-        font_size:dp(18)
-    SponsorImage
-        on_release:
-            import webbrowser
-            webbrowser.open(root.data['website'])
+        font_size: dp(14)
 
 <SponsorImage@ButtonBehavior+Image>
-    size_hint:1,.8
-    halign: 'center'
-    padding: dp(10), dp(10)
-    valign: 'middle'
-    allow_stretch:False
     source: self.parent.data['logo']
     on_release:
         import webbrowser
         webbrowser.open(self.parent.data['website'])
-
 ''')
 
     def on_enter(self, onsuccess=False):
@@ -82,5 +85,11 @@ class ScreenSponsor(Screen):
         for s in sponsors:
             bl = Factory.Sponsor(size_hint_y=.8 / len(sponsors), data=s)
             main_box.add_widget(bl)
-        footer = Factory.Footer()
-        main_box.add_widget(footer)
+
+        main_box.bind(minimum_height=main_box.setter('height'))
+
+        footer_box = self.ids.footer_box
+        footer_box.clear_widgets()
+        footer_box.add_widget(Factory.Footer())
+
+        Factory.Animation(opacity=1, d=.5).start(main_box)
