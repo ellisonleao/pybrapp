c142b8e6bed32acc4361d108fe8f72b22a6a1c7a
George Y. Kussumoto <georgeyk.dev@gmail.com>
2017-09-14 20:48:57 -0300

    Improve about screen, changed image and website label

--- pybr/uix/screens/screenabout.py
+++ pybr/uix/screens/screenabout.py
@@ -18,7 +18,7 @@ class ScreenAbout(Screen):
         ScrollGrid
             AsyncImage
                 id: imgbt
-                allow_stretch: True
+                allow_stretch: False
                 size_hint_y: None
                 height: dp(200)
             BackLabel
@@ -28,7 +28,6 @@ class ScreenAbout(Screen):
                 height: dp(45)
                 ActiveButton
                     id: but
-                    text: "Visit our website"
                     size_hint: None, None
                     width: dp(200)
                     center_x: comm_desc.center_x
@@ -48,6 +47,7 @@ class ScreenAbout(Screen):
         about = about.get('0.0.1')[0]
         imbt = self.ids.imgbt
         imbt.source = about['logo']
+        self.ids.but.text = about['website_label']
         self.ids.but.on_released = partial(webbrowser.open, about['website'])
 
         self.ids.comm_desc.text = about['about']
